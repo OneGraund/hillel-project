e3a4405305b1af303d75eaa22461a0b3c41fe2e7
OneGraund <vovangaunder@gmail.com>
2020-01-14 08:50:17 +0300

    First commit

---
+++ products/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+
+class Item(models.Model):
+    name = models.CharField(max_length=40) 					   #Наименование товара
+    description = models.CharField(max_length=200, default="") #Описание товара
+    price = models.IntegerField()			  				   #Цена товара
+    quantity = models.IntegerField(default=0) 				   #Количество товара на складе
+    def __str__(self):
+    	return (f"{self.id}: {self.name}") 
